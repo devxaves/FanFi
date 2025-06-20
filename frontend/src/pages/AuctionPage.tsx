333a33b8f9ee208676835ee3692d2a51930a59fd
Anirban Bandyopadhyay <136166021+anirban12x@users.noreply.github.com>
2025-06-21 03:37:24 +0530

    basic aptos push

---
+++ frontend/src/pages/AuctionPage.tsx
@@ -0,0 +1,217 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { Typography, Card, Row, Col, Button, Pagination, message, Spin, Tag } from "antd";
+import { AptosClient } from "aptos";
+import { MARKET_PLACE_ADDRESS, MARKET_PLACE_NAME } from "../Constants";
+import Meta from "antd/es/card/Meta";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useNavigate } from "react-router-dom";
+import PlaceBidModal from "../components/PlaceBidModal";
+import { rarityColors, rarityLabels, truncateAddress } from "../utils/rarityUtils";
+import { Auction } from "../types/nftType";
+import { fetchNFTDataUtil } from "../utils/fetchNFTData";
+import { client } from "../utils/aptoClientUtil";
+
+const { Title } = Typography;
+
+const AuctionsPage = () => {
+  const { account } = useWallet();
+  const [nftAuctions, setNftAuctions] = useState<any[]>([]);
+  const [totalAuctions, setTotalAuctions] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedAuction, setSelectedAuction] = useState<any | null>(null);
+  const [isBidModalVisible, setIsBidModalVisible] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const navigate = useNavigate();
+  const pageSize = 8;
+
+  const fetchAuctions = useCallback(async () => {
+    if (!account) return;
+    try {
+      setLoading(true);
+      const offset = ((currentPage - 1) * pageSize).toString();
+      const limit = pageSize.toString();
+
+      const auctionDataResponse = await client.view({
+        function: `${MARKET_PLACE_ADDRESS}::${MARKET_PLACE_NAME}::get_active_auctions`,
+        arguments: [MARKET_PLACE_ADDRESS, limit, offset],
+        type_arguments: [],
+      });
+
+      const auctionsList = Array.isArray(auctionDataResponse[0]) ? auctionDataResponse[0] : [];
+      setTotalAuctions(auctionsList.length);
+
+      if (auctionsList.length === 0) {
+        setNftAuctions([]);
+        return;
+      }
+
+      const auctionPromises = auctionsList.map(async (auction: any) => {
+        const { nft_id } = auction;
+        const nftMetadata = await fetchNFTDataUtil(nft_id, account.address, client);
+        return nftMetadata;
+      });
+
+      const auctionsWithMetadata = await Promise.all(auctionPromises);
+      setNftAuctions(auctionsWithMetadata);
+    } catch (error) {
+      console.error("Error fetching auctions:", error);
+      message.error("Failed to fetch auctions.");
+    } finally {
+      setLoading(false);
+    }
+  }, [currentPage, account]);
+
+  useEffect(() => {
+    fetchAuctions();
+  }, [fetchAuctions, currentPage]);
+
+  const showModal = (auction: Auction) => {
+    setSelectedAuction(auction);
+    setIsBidModalVisible(true);
+  };
+
+  const handleEndAuction = async (nftId: number) => {
+    if (!account) return;
+
+    try {
+      const entryFunctionPayload = {
+        function: `${MARKET_PLACE_ADDRESS}::${MARKET_PLACE_NAME}::end_auction`,
+        type_arguments: [],
+        arguments: [MARKET_PLACE_ADDRESS, nftId],
+      };
+
+      const txnResponse = await (window as any).aptos.signAndSubmitTransaction(entryFunctionPayload);
+      await client.waitForTransaction(txnResponse.hash);
+      message.success(`Auction ended successfully!`);
+      fetchAuctions();
+    } catch (error) {
+      console.error("Error ending auction:", error);
+      message.error("Failed to end auction!");
+    }
+  };
+
+  if (loading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Spin size="large" />
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <Title level={2} style={{ marginBottom: "20px" }}>Ongoing Auctions</Title>
+
+      <Row
+        gutter={[24, 24]}
+        style={{
+          marginTop: 20,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {nftAuctions.map((nft) => (
+          <Col
+            key={nft.id}
+            xs={24}
+            sm={12}
+            md={8}
+            lg={6}
+            xl={6}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Card
+              hoverable
+              style={{
+                width: "100%",
+                maxWidth: "240px",
+                margin: "0 auto",
+              }}
+              extra={
+                <Tag
+                  color={rarityColors[nft.rarity]}
+                  style={{ fontSize: "14px", fontWeight: "bold", marginBottom: "10px" }}
+                >
+                  {rarityLabels[nft.rarity]}
+                </Tag>
+              }
+              cover={<img alt={nft.name} src={nft.uri} />}
+              actions={[
+                nft.owner === account?.address ? (
+                  <Button
+                    key="self-bid"
+                    type="link"
+                    onClick={() => showModal(nft)}
+                    disabled={nft.auction.isExpired}
+                  >
+                    Self Bid
+                  </Button>
+                ) : (
+                  <Button
+                    key="place-bid"
+                    type="link"
+                    onClick={() => showModal(nft)}
+                    disabled={nft.auction.isExpired}
+                  >
+                    Place Bid
+                  </Button>
+                ),
+                nft.owner === account?.address && (
+                  <Button
+                    key="end-auction"
+                    disabled={!nft.auction.isExpired}
+                    type="link"
+                    danger
+                    onClick={() => handleEndAuction(nft.id)}
+                  >
+                    End Auction
+                  </Button>
+                ),
+              ]}
+            >
+              <div onClick={() => navigate(`/nft-detail/${nft.id}`)}>
+                <h4>Auction Details:</h4>
+                <p>Auction End Time: {nft.auction.end_time}</p>
+                <p>Starting Bid: {nft.auction.starting_bid} APT</p>
+                <p>Highest Bid: {nft.auction.highest_bid} APT</p>
+                <hr />
+                <h4>{nft.name}</h4>
+                <p>{nft.description}</p>
+                <p style={{ fontSize: "12px" }}>
+                  Owner: {nft.owner === account?.address && "You | "}
+                  {truncateAddress(nft.owner)}
+                </p>
+              </div>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      <div style={{ marginTop: 30, marginBottom: 30 }}>
+        <Pagination
+          current={currentPage}
+          pageSize={pageSize}
+          total={totalAuctions}
+          onChange={(page) => setCurrentPage(page)}
+          style={{ display: "flex", justifyContent: "center" }}
+        />
+      </div>
+
+      <PlaceBidModal
+        isVisible={isBidModalVisible}
+        onClose={() => setIsBidModalVisible(false)}
+        nftDetails={selectedAuction?.nftMetadata || {}}
+        auction={selectedAuction}
+        onRefresh={fetchAuctions}
+      />
+    </div>
+  );
+};
+
+export default AuctionsPage;
