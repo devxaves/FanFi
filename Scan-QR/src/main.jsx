3713045c0084ce59e729ca53c603f3879a23b07b
SUJAY DEY <149602382+sujayx07@users.noreply.github.com>
2025-06-21 17:46:13 +0530

    Main changes of QR

--- Scan-QR/src/main.jsx
+++ Scan-QR/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import QRScanner from './components/QRScanner';
 import './index.css'
 import App from './App.jsx'
-import QRScanner from './QRScanner.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <QRScanner/>
+     <QRScanner />
   </StrictMode>,
 )
