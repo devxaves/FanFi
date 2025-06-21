5b123a7e262a448da36a29d19cbff1443ccbaf8b
SUJAY DEY <149602382+sujayx07@users.noreply.github.com>
2025-06-21 14:15:34 +0530

    Update 2.0

--- QR-Reader/my-app/postcss.config.cjs
+++ QR-Reader/my-app/postcss.config.cjs
@@ -1,10 +1,7 @@
-// postcss.config.js
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-/** @type {import('postcss').Config} */
-const config = {
-  plugins: [tailwindcss, autoprefixer],
+// postcss.config.cjs
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // use this instead of `tailwindcss`
+    autoprefixer: {},
+  },
 };
-
-export default config;
